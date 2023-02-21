ffe7287d6bb1adf1a9059b2cca254d62edf0f902
Macleanan <j.macleanan@gmail.com>
2023-02-21 19:00:12 +0000

    Animation Build

--- Le Flap De Bird CS Scripts/BirdScript.cs
+++ Le Flap De Bird CS Scripts/BirdScript.cs
@@ -9,13 +9,16 @@ public class BirdScript : MonoBehaviour
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
+    public Animator wingAnimator; 
 
 
     // Start is called before the first frame update
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        wingAnimator.SetBool("AnimBirdIsAlive", true);
     }
+
     //Commented the explaination in Pipe middle script
 
     // Update is called once per frame
@@ -32,6 +35,7 @@ public class BirdScript : MonoBehaviour
         {
             logic.gameOver();
             birdIsAlive = false;
+            wingAnimator.SetBool("AnimBirdIsAlive", false);
         }
         //runs gameover Ui script, makes the birds bool say false
         //This happens if the bird is above 15 Y height or below -14.5 Y height
@@ -41,6 +45,7 @@ public class BirdScript : MonoBehaviour
         logic.collisionSFX.Play();
         logic.gameOver();
         birdIsAlive = false;
+        wingAnimator.SetBool("AnimBirdIsAlive", false);
     }
     //runs gameover Ui script, makes the birds bool say false
 }
